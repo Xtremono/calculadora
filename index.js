683c6f6807962aeac4bc76c4f1d23037aaf68d81
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-09-19 12:15:23 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const screen = document.getElementById("display");
+const buttons = document.querySelectorAll(".buttons");
+
+buttons.forEach(boton => {
+  boton.addEventListener("click", () => {
+
+  })
+})
